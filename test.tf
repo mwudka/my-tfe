1c762a0e45c6b0100ed221d4b8188c8d1acbadaa
Manuel Wudka-Robles <mwudka@gmail.com>
2019-09-19 15:30:34 -0700

    Testing

---
+++ test.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "remote" {
+    hostname = "tfe-zone-60f6da41.ngrok.io"
+    organization = "hashicorp_v2"
+
+    workspaces {
+      name = "scm-testing"
+    }
+  }
+}
+
+resource "random_id" "dummy-value" {
+    byte_length = 10
+    count = 100
+}
+
+# data "http" "slow-http-requests" {
+#     count = 1
+#     url = "https://httpbin.org/delay/10"
+# }
