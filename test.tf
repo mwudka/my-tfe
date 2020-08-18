d4e31f4c2ed3bdf0692bd8d0dd3e5aa6a160f9cc
Manuel Wudka-Robles <mwudka@gmail.com>
2020-08-18 16:07:37 -0700

    Change

--- test.tf
+++ test.tf
@@ -1,8 +1,5 @@
 //--------------------------------------------------------------------
 // Modules
-module "animal" {
-  source  = "tfe-zone-60f6da41.ngrok.io/otherorg/animal/demo"
-  version = "1.0.0"
-
-  name = "testing"
+resource "random_id" "server" {
+  byte_length = 8
 }
