27e4522c2cf1f36967cbf38315d13fc842d745b9
Manuel Wudka-Robles <mwudka@gmail.com>
2021-01-07 16:49:41 -0800

    Update test.tf

--- test.tf
+++ test.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
  region = "eu-west-2"
- access_key = var.aws_access_key
- secret_key = var.aws_secret_key
+ access_key = "foo"
+ secret_key = "bar"
 }
 
 module "vpc" {
