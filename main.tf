354ca89b4dc3fb8fad914f11acd0785f4bf6653c
Manuel Wudka-Robles <mwudka@gmail.com>
2021-11-08 18:11:31 -0800

    Always regenerate random

--- main.tf
+++ main.tf
@@ -4,13 +4,29 @@ terraform {
       source = "hashicorp/random"
       version = "3.1.0"
     }
+    http = {
+      source = "hashicorp/http"
+      version = "2.1.0"
+    }
   }
 }
 
+data "http" "random" {
+  url = "http://www.randomnumberapi.com/api/v1.0/uuid"
+
+}
 resource "random_id" "server" {
     byte_length = 8
+
+    keepers = {
+      "uuid" = data.http.random.body
+    }
 }
 
 output "random_hex" {
   value = random_id.server.hex
+}
+
+output "random_http_uuid" {
+  value = data.http.random.body
 }
